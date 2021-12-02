664c66a6533badfb50af8696869ab988be110eea
Fred Morcos <fm@fredmorcos.com>
2021-12-02 21:00:48 +0100

    2.5m iterations/s

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,31 @@
-use std::time::Instant;
+#![warn(clippy::all)]
 
 use rand::distributions::{DistIter, Standard};
 use rand::prelude::StdRng;
 use rand::{Rng, SeedableRng};
 use sha1::{Digest, Sha1};
+use std::error::Error;
+use std::time::Instant;
 
-fn random_string(s: &mut Vec<u8>, len: usize, rng: &mut DistIter<Standard, StdRng, u8>) {
-    fn pred(&c: &u8) -> bool {
-        c != b'\r' && c != b'\t' && c != b'\n' && c != b' '
+pub type Res<T> = Result<T, Box<dyn Error>>;
+
+fn random_string(s: &mut String, len: usize, rng: &mut DistIter<Standard, StdRng, char>) {
+    fn pred(&c: &char) -> bool {
+        c != '\r' && c != '\t' && c != '\n' && c != ' '
     }
 
     s.clear();
     s.extend(rng.by_ref().filter(pred).take(len));
 }
 
-// fn hash(hasher: &mut Sha1, suffix: &str, hash: &mut [u8]) {
-// }
+fn hash(mut hasher: Sha1, suffix: &str, hash: &mut [u8]) -> Res<()> {
+    hasher.update(suffix);
+    let hashed = &hasher.finalize()[..];
+    hex::encode_to_slice(hashed, hash)?;
+    Ok(())
+}
 
-fn matches_difficulty(authdata: &str, suffix: &[u8], difficulty: u8, hex_hash: &mut [u8]) -> bool {
+fn matches_difficulty(hash: &[u8], difficulty: u8) -> bool {
     static DIFFICULTY_TABLE: [&[u8]; 9] = [
         b"0",
         b"00",
@@ -30,41 +38,43 @@ fn matches_difficulty(authdata: &str, suffix: &[u8], difficulty: u8, hex_hash: &
         b"000000000",
     ];
 
-    let mut hasher = Sha1::default();
-    hasher.update(authdata);
-    hasher.update(suffix);
-    let hashed = &hasher.finalize()[..];
-    hex::encode_to_slice(hashed, hex_hash).unwrap();
-
-    let matches = hex_hash.starts_with(DIFFICULTY_TABLE[usize::from(difficulty - 1)]);
-    if matches {
-        println!(
-            "Found string with authentication hash `{}` that matches difficulty {}",
-            std::str::from_utf8(hex_hash).unwrap(),
-            difficulty
-        );
-    }
-    matches
+    hash.starts_with(DIFFICULTY_TABLE[usize::from(difficulty - 1)])
 }
 
-fn main() {
+fn main() -> Res<()> {
     const LEN: usize = 32;
     const HEX_HASH_LEN: usize = 40;
     let diff = 7;
 
-    let mut rng: DistIter<Standard, StdRng, u8> = StdRng::from_entropy().sample_iter(Standard);
+    let mut rng: DistIter<Standard, StdRng, char> = StdRng::from_entropy().sample_iter(Standard);
     let authdata = "kHtMDdVrTKHhUaNusVyBaJybfNMWjfxnaIiAYqgfmCTkNKFvYGloeHDHdsksfFla";
-    let mut suffix = Vec::with_capacity(LEN);
+    let mut suffix = String::with_capacity(LEN);
     let mut hex_hash = [0u8; HEX_HASH_LEN];
+
+    let mut base_hasher = Sha1::default();
+    base_hasher.update(authdata);
+
     let start = Instant::now();
+
     let mut iters = 0;
-    for _ in 0..10_000_000 {
+    for _ in 0..100_000_000 {
         iters += 1;
+
+        let hasher = base_hasher.clone();
         random_string(&mut suffix, LEN, &mut rng);
-        if matches_difficulty(authdata, &suffix, diff, &mut hex_hash) {
+        hash(hasher, &suffix, &mut hex_hash)?;
+
+        if matches_difficulty(&hex_hash, diff) {
+            println!(
+                "Found string with authentication hash `{}` that matches difficulty {}",
+                std::str::from_utf8(&hex_hash).unwrap(),
+                diff
+            );
+
             break;
         }
     }
+
     let duration = Instant::now().duration_since(start);
     let iters_per_micro = f64::from(iters) / f64::from(duration.as_micros() as i32);
     let iters_per_sec = iters_per_micro * 1_000_000.0;
@@ -74,4 +84,6 @@ fn main() {
         iters,
         iters_per_sec
     );
+
+    Ok(())
 }
