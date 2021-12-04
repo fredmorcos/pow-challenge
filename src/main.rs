97e655f8879acbd7d198301b147686f4b9445c1e
Fred Morcos <fm@fredmorcos.com>
2021-12-04 19:00:39 +0100

    Comments & cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![warn(clippy::all)]
 
+use humantime::format_duration as humantime;
 use rand::distributions::{DistIter, Uniform};
 use rand::{thread_rng, Rng, SeedableRng};
 use rand_xoshiro::Xoshiro128PlusPlus;
@@ -10,20 +11,31 @@ use sha1::{Digest, Sha1};
 use std::error::Error;
 use std::sync::atomic::{AtomicBool, AtomicUsize};
 use std::sync::Arc;
-#[cfg(feature = "timeit")]
-use std::time::Duration;
 use std::time::Instant;
+
+#[cfg(feature = "stats")]
+use std::time::Duration;
 
 pub type Res<T> = Result<T, Box<dyn Error>>;
 
 type Distribution = rand::distributions::Uniform<u8>;
 type BytesRng = DistIter<Distribution, Xoshiro128PlusPlus, u8>;
 
+// Create a PRNG seeded from a CSRNG. PRNGs are ~3 times faster and we can re-seed every
+// once in a while.
+fn create_rng(thread_idx: usize) -> BytesRng {
+  Xoshiro128PlusPlus::from_rng(thread_rng())
+    .unwrap_or_else(|_| panic!("Thread {}: Could not create RNG", thread_idx))
+    .sample_iter(Uniform::from(33..127))
+}
+
 fn random_string<const LEN: usize>(s: &mut Vec<u8>, rng: &mut BytesRng) {
   s.clear();
   s.extend(rng.take(LEN));
 }
 
+// We use the sha-1 crate instead of OpenSSL because it's faster, see
+// https://github.com/fredmorcos/sha1_rs_vs_openssl
 fn hash(mut hasher: Sha1, suffix: &[u8], hash: &mut GenericArray<u8, U20>) {
   hasher.update(suffix);
   hasher.finalize_into(hash);
@@ -31,51 +43,78 @@ fn hash(mut hasher: Sha1, suffix: &[u8], hash: &mut GenericArray<u8, U20>) {
 
 #[rustfmt::skip]
 fn matches_difficulty<const N: usize>(hash: &[u8]) -> bool {
-    if N == 0 {
-        true
-    } else if N == 1 {
-        *unsafe { hash.get_unchecked(0) } < 16
-    } else if N == 2 {
-        *unsafe { hash.get_unchecked(0) } == 0
-    } else if N == 3 {
-        *unsafe { hash.get_unchecked(0) } == 0 &&
-        *unsafe { hash.get_unchecked(1) } < 16
-    } else if N == 4 {
-        *unsafe { hash.get_unchecked(0) } == 0 &&
-        *unsafe { hash.get_unchecked(1) } == 0
-    } else if N == 5 {
-        *unsafe { hash.get_unchecked(0) } == 0 &&
-        *unsafe { hash.get_unchecked(1) } == 0 &&
-        *unsafe { hash.get_unchecked(2) } < 16
-    } else if N == 6 {
-        *unsafe { hash.get_unchecked(0) } == 0 &&
-        *unsafe { hash.get_unchecked(1) } == 0 &&
-        *unsafe { hash.get_unchecked(2) } == 0
-    } else if N == 7 {
-        *unsafe { hash.get_unchecked(0) } == 0 &&
-        *unsafe { hash.get_unchecked(1) } == 0 &&
-        *unsafe { hash.get_unchecked(2) } == 0 &&
-        *unsafe { hash.get_unchecked(3) } < 16
-    } else if N == 8 {
-        *unsafe { hash.get_unchecked(0) } == 0 &&
-        *unsafe { hash.get_unchecked(1) } == 0 &&
-        *unsafe { hash.get_unchecked(2) } == 0 &&
-        *unsafe { hash.get_unchecked(3) } == 0
-    } else if N == 9 {
-        *unsafe{ hash.get_unchecked(0) } == 0 &&
-        *unsafe{ hash.get_unchecked(1) } == 0 &&
-        *unsafe{ hash.get_unchecked(2) } == 0 &&
-        *unsafe{ hash.get_unchecked(3) } == 0 &&
-        *unsafe{ hash.get_unchecked(4) } < 16
-    } else {
-        panic!("Unsupported difficulty level");
-    }
+  if N == 0 {
+    true
+  } else if N == 1 {
+    *unsafe { hash.get_unchecked(0) } < 16
+  } else if N == 2 {
+    *unsafe { hash.get_unchecked(0) } == 0
+  } else if N == 3 {
+    *unsafe { hash.get_unchecked(0) } == 0 &&
+    *unsafe { hash.get_unchecked(1) } < 16
+  } else if N == 4 {
+    *unsafe { hash.get_unchecked(0) } == 0 &&
+    *unsafe { hash.get_unchecked(1) } == 0
+  } else if N == 5 {
+    *unsafe { hash.get_unchecked(0) } == 0 &&
+    *unsafe { hash.get_unchecked(1) } == 0 &&
+    *unsafe { hash.get_unchecked(2) } < 16
+  } else if N == 6 {
+    *unsafe { hash.get_unchecked(0) } == 0 &&
+    *unsafe { hash.get_unchecked(1) } == 0 &&
+    *unsafe { hash.get_unchecked(2) } == 0
+  } else if N == 7 {
+    *unsafe { hash.get_unchecked(0) } == 0 &&
+    *unsafe { hash.get_unchecked(1) } == 0 &&
+    *unsafe { hash.get_unchecked(2) } == 0 &&
+    *unsafe { hash.get_unchecked(3) } < 16
+  } else if N == 8 {
+    *unsafe { hash.get_unchecked(0) } == 0 &&
+    *unsafe { hash.get_unchecked(1) } == 0 &&
+    *unsafe { hash.get_unchecked(2) } == 0 &&
+    *unsafe { hash.get_unchecked(3) } == 0
+  } else if N == 9 {
+    *unsafe{ hash.get_unchecked(0) } == 0 &&
+    *unsafe{ hash.get_unchecked(1) } == 0 &&
+    *unsafe{ hash.get_unchecked(2) } == 0 &&
+    *unsafe{ hash.get_unchecked(3) } == 0 &&
+    *unsafe{ hash.get_unchecked(4) } < 16
+  } else {
+    panic!("Unsupported difficulty level");
+  }
 }
 
-fn main() -> Res<()> {
-  const LEN: usize = 8;
+#[cfg(feature = "stats")]
+fn check_suffix(suffix: &[u8]) {
+  assert!(!suffix.contains(&b'\t'));
+  assert!(!suffix.contains(&b'\n'));
+  assert!(!suffix.contains(&b'\r'));
+  assert!(!suffix.contains(&b' '));
+}
 
-  let diff = 8;
+macro_rules! timeit {
+  ($e:expr => $var:ident) => {{
+    #[cfg(feature = "stats")]
+    let start = Instant::now();
+    let res = $e;
+    #[cfg(feature = "stats")]
+    {
+      $var += Instant::now().duration_since(start);
+    }
+    res
+  }};
+}
+
+#[cfg(feature = "stats")]
+fn setup_duration_counters() -> (Duration, Duration, Duration, Duration) {
+  let time_gen = Duration::ZERO;
+  let time_hashing = Duration::ZERO;
+  let time_matching = Duration::ZERO;
+  let time_stop = Duration::ZERO;
+  (time_gen, time_hashing, time_matching, time_stop)
+}
+
+fn get_matches_difficulty_pred(difficulty: usize) -> fn(&[u8]) -> bool {
   const DIFF_FUNC_TABLE: &[fn(&[u8]) -> bool] = &[
     matches_difficulty::<0>,
     matches_difficulty::<1>,
@@ -88,9 +127,18 @@ fn main() -> Res<()> {
     matches_difficulty::<8>,
     matches_difficulty::<9>,
   ];
-  let matches_difficulty_func = DIFF_FUNC_TABLE[diff];
+
+  DIFF_FUNC_TABLE[difficulty]
+}
+
+fn main() -> Res<()> {
+  const LEN: usize = 8;
 
   let authdata = "kHtMDdVrTKHhUaNusVyBaJybfNMWjfxnaIiAYqgfmCTkNKFvYGloeHDHdsksfFla";
+  let difficulty = 8;
+
+  let matches_difficulty_pred = get_matches_difficulty_pred(difficulty);
+
   let mut base_hasher = Sha1::default();
   base_hasher.update(authdata.as_bytes());
 
@@ -98,142 +146,114 @@ fn main() -> Res<()> {
   let nthreads = rayon::current_num_threads();
 
   let stop = Arc::new(AtomicBool::new(false));
-  let result_suffix = Arc::new(parking_lot::const_mutex(None));
+  let pow_result = Arc::new(parking_lot::const_mutex(None));
 
   let start = Instant::now();
   rayon::scope(|scope| {
-    for thread_i in 0..nthreads {
+    for thread_idx in 0..nthreads {
       let base_hasher = &base_hasher;
       let total_iters = total_iters.clone();
       let stop = stop.clone();
-      let result_suffix = result_suffix.clone();
+      let pow_result = pow_result.clone();
 
       scope.spawn(move |_| {
-        let mut rng = Xoshiro128PlusPlus::from_rng(thread_rng())
-          .unwrap()
-          .sample_iter(Uniform::from(33..127));
+        let mut rng = create_rng(thread_idx);
         let mut suffix = Vec::with_capacity(LEN);
         let mut hashed = Default::default();
 
-        #[cfg(feature = "timeit")]
-        let mut time_gen = Duration::ZERO;
-        #[cfg(feature = "timeit")]
-        let mut time_hashing = Duration::ZERO;
-        #[cfg(feature = "timeit")]
-        let mut time_checking_match = Duration::ZERO;
-        #[cfg(feature = "timeit")]
-        let mut time_stop_checking = Duration::ZERO;
+        #[cfg(feature = "stats")]
+        let (mut time_gen, mut time_hashing, mut time_matching, mut time_stop) = setup_duration_counters();
 
-        let mut iters = 0;
+        let mut iterations = 0;
         loop {
-          iters += 1;
+          iterations += 1;
 
-          #[cfg(feature = "timeit")]
-          let start = Instant::now();
-          random_string::<LEN>(&mut suffix, &mut rng);
-          #[cfg(feature = "timeit")]
-          {
-            time_gen += Instant::now().duration_since(start);
-          }
+          timeit!(random_string::<LEN>(&mut suffix, &mut rng) => time_gen);
+          timeit!(hash(base_hasher.clone(), &suffix, &mut hashed) => time_hashing);
+          let matches = timeit!(matches_difficulty_pred(&hashed) => time_matching);
 
-          #[cfg(feature = "check")]
-          {
-            assert!(!suffix.contains(&b'\t'));
-            assert!(!suffix.contains(&b'\n'));
-            assert!(!suffix.contains(&b'\r'));
-            assert!(!suffix.contains(&b' '));
-          }
-
-          #[cfg(feature = "timeit")]
-          let start = Instant::now();
-          hash(base_hasher.clone(), &suffix, &mut hashed);
-          #[cfg(feature = "timeit")]
-          {
-            time_hashing += Instant::now().duration_since(start);
-          }
-
-          #[cfg(feature = "timeit")]
-          let start = Instant::now();
-          let matches = matches_difficulty_func(&hashed);
-          #[cfg(feature = "timeit")]
-          {
-            time_checking_match += Instant::now().duration_since(start);
-          }
+          #[cfg(feature = "stats")]
+          check_suffix(&suffix);
 
           if matches {
+            // Threads check the stop flag every X iterations. This is obviously race-y
+            // but it's okay (i.e. it's still correct). The race happens when the current
+            // thread sets stop to true, stores the result and exits. Meanwhile, another
+            // thread could find a solution before its next scheduled stop check (which
+            // happens each X iterations), sets stop to true, stores its result and exits.
+
             stop.store(true, std::sync::atomic::Ordering::Release);
 
-            let mut result_suffix = if let Some(result_suffix) = result_suffix.try_lock() {
-              result_suffix
+            let mut pow_result = if let Some(pow_result) = pow_result.try_lock() {
+              pow_result
             } else {
-              // Another thread is writing a result they've found, the
-              // current thread can give up.
+              // Another thread is writing a result they've found, the current thread can
+              // give up.
               break;
             };
 
-            *result_suffix = Some(suffix);
-
-            println!("Thread {}: Found string ({}) {:?}", thread_i, hashed.len(), hashed);
+            *pow_result = Some(suffix);
 
             println!(
-              "Thread {}: Found string with authentication hash `{}` that matches difficulty {}",
-              thread_i,
+              "Thread {}: Found string hash ({} - {:?}) `{}` that matches difficulty {}",
+              thread_idx,
+              hashed.len(),
+              hashed,
               hex::encode(hashed),
-              diff
+              difficulty
             );
 
             break;
           }
 
-          #[cfg(feature = "timeit")]
-          let start = Instant::now();
-          let should_stop = iters % 10_000 == 0 && stop.load(std::sync::atomic::Ordering::Acquire);
-          #[cfg(feature = "timeit")]
-          {
-            time_stop_checking += Instant::now().duration_since(start);
-          }
-
-          if should_stop {
-            println!("Thread {}: stopping", thread_i);
+          let do_stop = iterations % 10_000 == 0;
+          let do_stop = do_stop && timeit!(stop.load(std::sync::atomic::Ordering::Acquire) => time_stop);
+          if do_stop {
+            println!("Thread {}: Stopping", thread_idx);
             break;
           }
+
+          // Re-seed with a CSRNG every Y iterations to search a different area in the
+          // search space.
+          if iterations % 100_000_000 == 0 {
+            println!("Thread {}: Reseeding", thread_idx);
+            rng = create_rng(thread_idx);
+          }
         }
 
-        #[cfg(feature = "timeit")]
-        {
-          println!(
-            "Thread {}: Gen({})  Hash({})  MatchCheck({})  StopCheck({})",
-            thread_i,
-            humantime::format_duration(time_gen),
-            humantime::format_duration(time_hashing),
-            humantime::format_duration(time_checking_match),
-            humantime::format_duration(time_stop_checking),
-          );
-        }
+        #[cfg(feature = "stats")]
+        println!(
+          "Thread {}: Gen({})  Hash({})  MatchCheck({})  StopCheck({})",
+          thread_idx,
+          humantime(time_gen),
+          humantime(time_hashing),
+          humantime(time_matching),
+          humantime(time_stop),
+        );
 
-        total_iters.fetch_add(iters, std::sync::atomic::Ordering::Release);
+        total_iters.fetch_add(iterations, std::sync::atomic::Ordering::Release);
       })
     }
   });
   let duration = Instant::now().duration_since(start);
 
-  if let Some(result_suffix) = &*result_suffix.lock() {
-    let mut hashed = Default::default();
-    hash(base_hasher, result_suffix, &mut hashed);
-    println!("Result = {}", hex::encode(hashed));
+  if let Ok(suffix) = Arc::try_unwrap(pow_result) {
+    if let Some(suffix) = suffix.into_inner() {
+      let mut hashed = Default::default();
+      hash(base_hasher, &suffix, &mut hashed);
+      println!("POW suffix = {:?}", suffix);
+      println!("POW hash = {}", hex::encode(hashed));
+    } else {
+      println!("No result found");
+    }
   } else {
-    println!("NO RESULT FOUND");
+    println!("No result found");
   }
 
   let total_iters = total_iters.load(std::sync::atomic::Ordering::SeqCst);
   let iters_per_micro = f64::from(total_iters as u32) / f64::from(duration.as_micros() as u32);
   let iters_per_sec = iters_per_micro * 1_000_000.0;
-  println!(
-    "{}: {} iterations: {} iterations/s",
-    humantime::format_duration(duration),
-    total_iters,
-    iters_per_sec
-  );
+  println!("{}: {} iterations: {} iterations/s", humantime(duration), total_iters, iters_per_sec);
 
   Ok(())
 }
